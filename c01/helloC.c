84c59de42f572bc8370c9061bd0c468dda48c880
PyMarcus <marcus-v@outlook.com>
2023-07-31 18:12:07 -0300

    hello world C

--- c01/helloC.c
+++ c01/helloC.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 //gcc -o hello -ansi  -std=c99 -pedantic-errors -Wall -Werror helloC.c
 //148
-int main(){
+int main(){   // _start assembly call main in C from standart library
     printf("hello, world!\n");
     return 0;
 }
