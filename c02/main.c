de53d408ad999bceb5cb7f5c01d3979fbaab9448
PyMarcus <marcus-v@outlook.com>
2023-07-31 19:18:05 -0300

    equivalent in c

---
+++ c02/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0x0
+#define END '\n'
+
+int main(){
+    char name[0xF];                                   // .bss section 
+    printf("What is your forename? %c", END); // eax, 0x4 register output operation
+    scanf("%s", name);                        // eax, 0x3 register input operation
+    printf("Your forename is: %s\n", name);
+    return EXIT_SUCCESS;
+}
