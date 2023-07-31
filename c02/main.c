9fbef6fbe324cbc32a358a4c19eda090512898f9
PyMarcus <marcus-v@outlook.com>
2023-07-31 19:18:56 -0300

    input and output in c

--- c02/main.c
+++ c02/main.c
@@ -3,6 +3,7 @@
 #define EXIT_SUCCESS 0x0
 #define END '\n'
 
+
 int main(){
     char name[0xF];                                   // .bss section 
     printf("What is your forename? %c", END); // eax, 0x4 register output operation
